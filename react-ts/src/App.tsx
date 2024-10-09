6bc9eab2d4d63a5ea358afae92227008454a6d83
Ali <dzmohamedali31@gmail.com>
2024-10-09 18:01:57 +0100

    first commit, hello world component

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-9xl mb-10 font-bold text-slate-800 bg-slate-200">
+      Hello world
+      </h1>
+      <div className="flex  bg-slate-900">
+        <p className="flex-auto text-xl text-white m-2">just some words...</p>
+        <p className="flex-auto text-xl text-white m-2">Other words...</p>
+      </div>
+      
+    </>
+     
+  )
+}
+
+export default App
